8dbc65ca562263991d9f62640d2129c93a0d4d85
kromerh <HeikoKromer@user-goldens-macbook-pro.home>
2022-05-30 14:52:22 +0200

    sh in repo

--- src/python/constants.py
+++ src/python/constants.py
@@ -2,9 +2,11 @@
 
 # Home directory on the RPi
 HOME = '/home/wilson'
+REPO = 'wilsonisahamster'
+PYTHON = 'src/python'
 # Location of the .sh file on the RPi (copied from this repo)
 FILENAME_RUN_HAMSTERWHEEL = 'run_hamsterwheel.sh'
-HAMSTERWHEEL_PATH = f'{HOME}/{FILENAME_RUN_HAMSTERWHEEL}'
+HAMSTERWHEEL_PATH = f'{HOME}/{REPO}/{PYTHON}/{FILENAME_RUN_HAMSTERWHEEL}'
 
 # Log files
 # Log file for the hamsterwheel sensor code
@@ -19,7 +21,7 @@ DATABASE = 'cryptohamster'
 HOST_LOCAL = 'localhost'  # On the RPi local
 HOST_REMOTE = 'wilsonisahamster'  # Sometimes does not work: then use 192.168.1.105
 PORT = 3306
-FULL_PATH_TO_CREDENTIALS = '/home/wilson/credentials.cred'
+FULL_PATH_TO_CREDENTIALS = f'{HOME}/credentials.cred'
 
 # Table for the wheel data
 TABLE_HAMSTERWHEEL = 'hamsterwheel'
