64529476f2cdbf9eb34898c46bb14c01bfac5703
kromerh <HeikoKromer@user-goldens-macbook-pro.home>
2022-05-30 14:18:08 +0200

    file structure

--- src/bash/run_hamsterwheel_handler.sh
+++ src/bash/run_hamsterwheel_handler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 30 &&
 cd /home/wilson/wilsonisahamster &&
-/home/wilson/hamsterwheelcaster/bin/python /home/wilson/wilsonisahamster/src/hamsterwheel_handler.py
+/home/wilson/hamsterwheelcaster/bin/python /home/wilson/wilsonisahamster/src/python/hamsterwheel_handler.py
