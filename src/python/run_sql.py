f24c939ac85e8e872ec32e994f7caab1493b24f4
kromerh <HeikoKromer@tiberius.home>
2023-08-13 19:39:04 +0200

    sql

--- src/python/run_sql.py
+++ src/python/run_sql.py
@@ -37,4 +37,72 @@ cursor.execute(
 """
 )
 
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS log(
+    id INT NOT NULL AUTO_INCREMENT,
+    time TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    event VARCHAR(100) NOT NULL,
+    PRIMARY KEY ( id )
+    );
+    """
+)
+
+mysql_connection.commit()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS wallet(
+            id INT NOT NULL AUTO_INCREMENT,
+    currency_symbol VARCHAR(50) NOT NULL,
+    amount FLOAT NOT NULL,
+    PRIMARY KEY ( id )
+    );
+    """
+)
+
+mysql_connection.commit()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS price(
+    id INT NOT NULL AUTO_INCREMENT,
+    currency_symbol VARCHAR(50) NOT NULL,
+    price FLOAT NOT NULL,
+    PRIMARY KEY ( id )
+    );
+    """
+)
+
+mysql_connection.commit()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS orderbook(
+    id INT NOT NULL AUTO_INCREMENT,
+    time TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    wallet_id INT NOT NULL,
+    price FLOAT NOT NULL,
+    amount FLOAT NOT NULL,
+    currency VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( wallet_id ) REFERENCES wallet( id )
+    );
+    """
+)
+
+mysql_connection.commit()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS decision(
+    id INT NOT NULL AUTO_INCREMENT,
+    start_time TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_time TIMESTAMP(6),
+    number_hamsterwheels INT,
+    type VARCHAR(50) NOT NULL,
+    status VARCHAR(100),
+    result VARCHAR(50),
+    PRIMARY KEY ( id )
+    );
+"""
+)
+
 mysql_connection.close()
